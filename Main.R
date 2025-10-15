0ce9836187c1c1ec83e30a01bb11e51855be0aa3
JuliusGr01 <108138140+JuliusGr01@users.noreply.github.com>
2025-10-15 14:45:39 +0200

    Add files via upload

---
+++ Main.R
@@ -0,0 +1,6 @@
+########################
+######### MAIN #########
+########################
+
+# This file manages all the separate files and serves as a main file setting up the infrastructure
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# MP_LaborPol
+# MP_Labor_Pol
+This repo is collecting all codes belonging to the project "Monetary Policy Transmission with a Polarized Labor Market"
